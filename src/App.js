6f568ff91470f61d2ed429d23479a35eaff0de4a
Abhishek T S <tsabhishek1999@gmail.com>
2020-08-05 23:26:38 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+//imports from libraries
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 
-
-
+//imports from src
+import './App.css';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import NavBar from './Components/NavBar';
+
+//App component
 function App() {
   return (
     <div className="App">
+      <NavBar />
+
+      {/* router  */}
       <Switch>
       <Route exact path ="/" component ={Home} />
       <Route exact path ="/rooms" component ={Rooms} />
